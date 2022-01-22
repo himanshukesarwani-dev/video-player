735287a1f41461e157fb716657d4ee6200b0ecd7
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2022-01-22 23:50:31 +0530

    js complete

--- script.js
+++ script.js
@@ -17,17 +17,51 @@ function togglerVideoStatus(){
 
 // update play/pause icon
 function updatePlayIcon(){
-    return true
+if(video.paused){
+    play.innerHTML = '<i class= "fa fa-play fa-2x"></i>';
 }
 
+else {
+    play.innerHTML = '<i class= "fa fa-pause fa-2x"></i>';
+
+
+}
+
+}
 // update progress and timestamp
 function updateProgress(){
+progress.value = (video.currentTime/video.duration) * 100;
+
+ // Get the minutes
+ let mins = Math.floor(video.currentTime / 60);
+ if(mins < video.duration){
+   mins = '0' + String(mins);
+ }
+
+ // Get Seconds
+ let secs = Math.floor(video.currentTime % 60);
+ if(secs < video.duration){
+   secs = '0' + String(secs);
+ }
+
+ timestamp.innerHTML = `${mins}:${secs}`;
+
 
 }
 
 
 
 
+// set Video progress function 
+function setVideoProgress(){
+   video.currentTime = (+progress.value * video.duration)/100;
+}
+
+
+function stopVideo(){
+    video.currentTime = 0;
+    video.pause();
+}
 
 
 
